2f34aa38c07a1b5b2ac58e4da30fcf6b3c5c7051
ls1w <michelle_j.b@hotmail.com>
2023-12-05 09:29:07 -0300

    limpar and calcular ok

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,14 +1,50 @@
-'use strict'
+'use strict';
 
-let contador = 0;
-const slcNumero = parseInt(document.getElementById('slcNumero').value);
-let resultado;
-let linha;
+const resultadoExportado = document.getElementById('resultadoTabuada');
+const btnCalcular = document.getElementById('btn-calcular');
+const btnLimpar = document.getElementById('btn-limpar');
 
-function tabuadaWhile() {
-    while (contador <= 10) {
-        resultado = slcNumero + ' X ' + contador + ' = ' + (slcNumero * contador);
-        contador = contador * slcNumero
-    }
+
+
+
+function limparTabuada() {
+    resultadoExportado.innerHTML = '';
+    // location.reload();
 }
 
+function tabuadaWhile() {
+    limparTabuada();
+
+    const numTabuada = parseInt(document.getElementById('slcNumero').value);
+    let linha = '';
+    let resultado;
+
+    let contadorTabuada = 0;
+    while (contadorTabuada <= 10) {
+        resultado = numTabuada + ' X ' + contadorTabuada + ' = ' + (numTabuada * contadorTabuada);
+        linha += '<tr><td>' + resultado + '</td></tr>';
+        contadorTabuada++;
+    };
+
+    resultadoExportado.innerHTML = linha;
+};
+
+function tabuadaFor() {
+    limparTabuada();
+
+    const numTabuada = parseInt(document.getElementById('slcNumero').value);
+    let linha = '';
+    let resultado;
+
+    for (let contadorTabuada = 0; contadorTabuada <= 10; contadorTabuada++) {
+        resultado = numTabuada + ' X ' + contadorTabuada + ' = ' + (numTabuada * contadorTabuada);
+        linha += '<tr><td>' + resultado + '</td></tr>';
+    };
+    resultadoExportado.innerHTML = linha;
+};
+
+
+
+
+btnCalcular.addEventListener('click' , tabuadaFor);
+btnLimpar.addEventListener('click' ,  limparTabuada);
